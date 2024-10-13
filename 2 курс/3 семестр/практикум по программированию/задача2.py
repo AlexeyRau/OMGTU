14a667265f89e37bc3a1c46bf6c36e5e67d39662
Rau Alexey <145207913+AlexeyRau@users.noreply.github.com>
2024-10-14 00:05:30 +0600

    Update задача2.py

--- 2 курс/3 семестр/практикум по программированию/задача2.py
+++ 2 курс/3 семестр/практикум по программированию/задача2.py
@@ -32,3 +32,32 @@ print("")
 y = 2 / (sp.sin(x) + 4)
 indefinite_integral = sp.integrate(y, x)
 print(f'Неопределённый интеграл: {indefinite_integral}')
+print("")
+
+# Целевая функция
+def objective(x):
+    return (x[0] - 4)**2 + (x[1] - 2)**2
+
+# Ограничения
+constraints = (
+    {'type': 'ineq', 'fun': lambda x: 4*x[0] + 2*x[1] - 11},
+    {'type': 'ineq', 'fun': lambda x: -2*x[0] - 7},
+)
+
+# Ограничения на переменные (x1 >= 0 и x2 >= 0)
+bounds = [(0, None), (0, None)]
+
+# Начальная точка для оптимизации
+initial_guess = [0, 0]
+
+# Решение задачи оптимизации
+result = minimize(objective, initial_guess, method='SLSQP', bounds=bounds, constraints=constraints)
+
+# Проверка успешности
+if result.success:
+    optimized_x = result.x
+    optimized_value = result.fun
+    print(f"Оптимальное значение: {optimized_value}")
+    print(f"Оптимальные решения: x1 = {optimized_x[0]}, x2 = {optimized_x[1]}")
+else:
+    print("Оптимизация не удалась:", result.message)
