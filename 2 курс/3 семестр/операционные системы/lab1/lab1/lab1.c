5b86453750f1779f013613d2633c687cb0ee38c6
Rau Alexey <rauleha2729@gmail.com>
2024-12-20 17:25:39 +0600

    update lab1

--- 2 курс/3 семестр/операционные системы/lab1/lab1/lab1.c
+++ 2 курс/3 семестр/операционные системы/lab1/lab1/lab1.c
@@ -4,27 +4,24 @@
 #define BUFFER_SIZE 1024
 
 int main() {
+	//Для корректного ввода и вывода русских букв
 	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
-	// Получение хэндлов стандартного ввода и вывода
+	
 	HANDLE hStdIn = GetStdHandle(STD_INPUT_HANDLE);
 	HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	// Проверка, связан ли стандартный вывод с консолью
+	// Чтобы в output.txt не записывалась лишняя информация
 	if (GetFileType(hStdOut) == FILE_TYPE_CHAR) {
-		// Вывод значений хэндлов только если вывод на консоль
 		printf("Хэндл стандартного ввода: %p\n", hStdIn);
 		printf("Хэндл стандартного вывода: %p\n", hStdOut);
 		printf("Ввод: ");
 	}
 
-	// Чтение данных из стандартного ввода
 	char buffer[BUFFER_SIZE];
 	DWORD bytesRead;
 	ReadFile(hStdIn, buffer, BUFFER_SIZE - 1, &bytesRead, NULL);
 	buffer[bytesRead] = '\0';
-
-	// Вывод введенных данных
 	printf("Вы ввели: %s", buffer);
 
 	return 0;
