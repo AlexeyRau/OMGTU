43bee84e950479a303471151e49d1bf7dd4eeafd
Rau Alexey <rauleha2729@gmail.com>
2024-12-20 16:29:25 +0600

    update lab1

--- 2 курс/3 семестр/операционные системы/lab1/lab1/lab1.c
+++ 2 курс/3 семестр/операционные системы/lab1/lab1/lab1.c
@@ -3,26 +3,25 @@
 
 int main() {
     // Получение хэндлов стандартного ввода и вывода
-    HANDLE hStdIn = GetStdHandle(STD_INPUT_HANDLE);
-    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    // Вывод значений хэндлов
-    printf("Standard Input Handle: %p\n", hStdIn);
-    printf("Standard Output Handle: %p\n", hStdOut);
+    // Вывод хэндлов
+    printf("Standard Input Handle: %p\n", hStdin);
+    printf("Standard Output Handle: %p\n", hStdout);
 
     // Приглашение для ввода
-    WriteFile(hStdOut, "Enter some text: ", 17, NULL, NULL);
+    WriteFile(hStdout, "Enter some text: ", 17, NULL, NULL);
 
-    // Чтение ввода
+    // Чтение данных из стандартного ввода
     char buffer[256];
     DWORD bytesRead;
-    ReadFile(hStdIn, buffer, sizeof(buffer) - 1, &bytesRead, NULL);
+    ReadFile(hStdin, buffer, sizeof(buffer) - 1, &bytesRead, NULL);
     buffer[bytesRead] = '\0'; // Добавление завершающего нуля
 
     // Вывод введенного текста
-    WriteFile(hStdOut, "You entered: ", 13, NULL, NULL);
-    WriteFile(hStdOut, buffer, bytesRead, NULL, NULL);
-    WriteFile(hStdOut, "\n", 1, NULL, NULL);
+    WriteFile(hStdout, "You entered: ", 13, NULL, NULL);
+    WriteFile(hStdout, buffer, bytesRead, NULL, NULL);
 
     return 0;
 }
