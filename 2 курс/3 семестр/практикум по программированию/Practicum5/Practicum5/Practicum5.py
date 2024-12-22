26eb9c388fca1672f56deb4868286eee43ea475b
Rau Alexey <rauleha2729@gmail.com>
2024-12-22 15:28:28 +0600

    Add Practicum5.1

--- 2 курс/3 семестр/практикум по программированию/Practicum5/Practicum5/Practicum5.py
+++ 2 курс/3 семестр/практикум по программированию/Practicum5/Practicum5/Practicum5.py
@@ -1,1 +1,10 @@
+﻿import pandas as pd
 
+# Создаем датафрейм
+data = {
+    'Name': ['Strom, Mrs. Wilhelm (Elna Matilda Persson)', 'Navratil, Mr. Michel ("Louis M Hoffman")', 'Minahan, Miss. Daisy E'],
+    'Age': [29, 36.5, 33],
+    'Sex': ['female', 'male', 'female']
+}
+df1 = pd.DataFrame(data)
+print(df1)
