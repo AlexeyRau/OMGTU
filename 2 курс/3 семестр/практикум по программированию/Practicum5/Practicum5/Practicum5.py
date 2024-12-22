fe5120a045a28d58655e039deec4f474575b351c
Rau Alexey <rauleha2729@gmail.com>
2024-12-22 15:35:30 +0600

    Update Practicum5

--- 2 курс/3 семестр/практикум по программированию/Practicum5/Practicum5/Practicum5.py
+++ 2 курс/3 семестр/практикум по программированию/Practicum5/Practicum5/Practicum5.py
@@ -1,13 +1,16 @@
 ﻿import pandas as pd
 
-# Создаем датафрейм
 data = {
     'Name': ['Strom, Mrs. Wilhelm (Elna Matilda Persson)', 'Navratil, Mr. Michel ("Louis M Hoffman")', 'Minahan, Miss. Daisy E'],
     'Age': [29, 36.5, 33],
     'Sex': ['female', 'male', 'female']
 }
 df1 = pd.DataFrame(data)
+print('Задание 1')
 print(df1)
-# Загружаем данные из файла
+print('')
+
 df2 = pd.read_csv('titanic_csv.csv', delimiter=';')
+print('Задание 2')
 print(df2)
+print('')
