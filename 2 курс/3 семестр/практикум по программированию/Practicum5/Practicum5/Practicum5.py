6222b32d89ed109af114bf5df24ef68de58a2fea
Rau Alexey <rauleha2729@gmail.com>
2024-12-22 15:39:27 +0600

    Add Practicum5.3

--- 2 курс/3 семестр/практикум по программированию/Practicum5/Practicum5/Practicum5.py
+++ 2 курс/3 семестр/практикум по программированию/Practicum5/Practicum5/Practicum5.py
@@ -14,3 +14,9 @@ df2 = pd.read_csv('titanic_csv.csv', delimiter=';')
 print('Задание 2')
 print(df2)
 print('')
+
+url = 'https://gist.githubusercontent.com/zaryanezrya/8b4ef51c707cb16d5e88a44dc00a1bb2/raw/41230f49c6268e072dbf102672f670be256922ab/gistfile1.txt'
+df3 = pd.read_csv(url, delimiter=',')
+print('Задание 3')
+print(df3)
+print('')
