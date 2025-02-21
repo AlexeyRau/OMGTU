1a623e64ef84a7cb74f721db4462274ae5733037
Rau Alexey <rauleha2729@gmail.com>
2025-02-22 02:18:54 +0600

    feat: Добавлен поиск цикла

--- 2 курс/4 семестр/Методы оптимизации для машинного обучения/transport task/transport task/transport_task.py
+++ 2 курс/4 семестр/Методы оптимизации для машинного обучения/transport task/transport task/transport_task.py
@@ -1,3 +1,4 @@
+from itertools import cycle
 import numpy as np
 
 # Матрица стоимости
@@ -53,3 +54,90 @@ X = north_west_corner(a.copy(), b.copy())
 print("Начальная транспортная таблица:")
 print(X)
 
+def calculate_potentials(X, C):
+    m, n = X.shape
+    u = np.full(m, np.nan)  # Инициализируем u как массив NaN
+    v = np.full(n, np.nan)  # Инициализируем v как массив NaN
+    
+    # Начинаем с u[0] = 0
+    u[0] = 0
+
+    # Список базисных клеток
+    basis_cells = [(i, j) for i in range(m) for j in range(n) if X[i, j] > 0]
+    
+
+    while basis_cells:
+        for i, j in basis_cells:
+            if not np.isnan(u[i]) and np.isnan(v[j]):
+                v[j] = C[i, j] - u[i]
+                basis_cells.remove((i, j))
+            elif not np.isnan(v[j]) and np.isnan(u[i]):
+                u[i] = C[i, j] - v[j]
+                basis_cells.remove((i, j))
+    
+    return u, v
+
+u, v = calculate_potentials(X, C)
+print("Потенциалы u:", u)
+print("Потенциалы v:", v)
+
+def find_entering_cell(X, C, u, v):
+    max_diff = 0
+    entering_cell = None
+    
+    # Проходим по всем небазисным клеткам
+    for i in range(X.shape[0]):
+        for j in range(X.shape[1]):
+            if X[i, j] == 0:  # Небазисная клетка
+                diff = u[i] + v[j] - C[i, j]
+                if diff > max_diff:
+                    max_diff = diff
+                    entering_cell = (i, j)
+    
+    return entering_cell
+
+entering_cell = find_entering_cell(X, C, u, v)
+
+print(f"Начальная точка: {entering_cell}")
+
+def find_cycle(X, entering_cell):
+    m, n = X.shape
+    i0, j0 = entering_cell
+    path = [(i0, j0)]  # Начинаем с входящей клетки
+    visited = set()    # Множество посещённых клеток
+
+    def dfs(i, j, step):
+        if (i, j) == (i0, j0) and step > 0:
+            return True  # Цикл замкнулся
+
+        if step % 2 == 0:
+            # Двигаемся по строке (ищем столбцы)
+            for j_next in range(n):
+                if j_next != j and (X[i, j_next] > 0 or (i, j_next) == (i0, j0)):
+                    if (i, j_next) not in visited:
+                        visited.add((i, j_next))
+                        path.append((i, j_next))
+                        if dfs(i, j_next, step + 1):
+                            return True
+                        path.pop()
+                        visited.remove((i, j_next))
+        else:
+            # Двигаемся по столбцу (ищем строки)
+            for i_next in range(m):
+                if i_next != i and (X[i_next, j] > 0 or (i_next, j) == (i0, j0)):
+                    if (i_next, j) not in visited:
+                        visited.add((i_next, j))
+                        path.append((i_next, j))
+                        if dfs(i_next, j, step + 1):
+                            return True
+                        path.pop()
+                        visited.remove((i_next, j))
+        return False
+
+    # Запуск поиска
+    dfs(i0, j0, 0)
+    return path
+
+# Пример использования
+cycle = find_cycle(X, entering_cell)
+print("Цикл:", cycle)
