9e5124597270a6c2669854728e2084512a2addb3
Rau Alexey <rauleha2729@gmail.com>
2025-03-22 09:49:42 +0600

    Готовы полностью два первых алгоритма

--- 2 курс/4 семестр/Методы оптимизации для машинного обучения/lab1/lab1.py
+++ 2 курс/4 семестр/Методы оптимизации для машинного обучения/lab1/lab1.py
@@ -15,35 +15,37 @@ def fibonacci_method(f, a, b, epsilon):
     def generate_fibonacci(n):
         fib = [1, 1]
         while fib[-1] < n:
-            fib.append(fib[-1] + fib[-2])
+            fib.append(fib[-1] + fib[-2]) 
         return fib
 
     fib = generate_fibonacci((b - a) / epsilon)
-    n = len(fib) - 1
+    n = len(fib) - 1  
 
-    x1 = a + (fib[n - 2] / fib[n]) * (b - a)
-    x2 = a + (fib[n - 1] / fib[n]) * (b - a)
-    f1, f2 = f(x1), f(x2)
+    L = b - a 
+    x1 = a + (fib[n - 1] / fib[n]) * L
+    x2 = b - (fib[n - 1] / fib[n]) * L
+    f1, f2 = f(x1), f(x2) 
 
     for k in range(1, n):
-        if f1 > f2:
-            a = x1
+        if f1 > f2: 
+            b = x1
             x1 = x2
             f1 = f2
-            x2 = a + (fib[n - k - 1] / fib[n - k]) * (b - a)
+            x2 = b - (fib[n - k - 1] / fib[n - k]) * (b - a)
             f2 = f(x2)
-        else:
-            b = x2
+        else:  
+            a = x2
             x2 = x1
             f2 = f1
-            x1 = a + (fib[n - k - 2] / fib[n - k]) * (b - a)
+            x1 = a + (fib[n - k - 1] / fib[n - k]) * (b - a)
             f1 = f(x1)
 
     x_min = (a + b) / 2
     return x_min, f(x_min)
 
 def golden_section_method(f, a, b, epsilon):
-    tau = (3 - 5 ** 0.5) / 2
+    tau = 0.618
+    print(f"tau {tau}")
     x1 = a + tau * (b - a)
     x2 = b - tau * (b - a)
     f1, f2 = f(x1), f(x2)
