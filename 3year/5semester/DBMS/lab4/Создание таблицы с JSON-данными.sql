0b454b92cb6ac7bf8689794654583a4326140007
Rau Alexey <rauleha2729@gmail.com>
2025-11-19 10:08:51 +0600

    fix

--- 3year/5semester/DBMS/lab4/Создание таблицы с JSON-данными.sql
+++ 3year/5semester/DBMS/lab4/Создание таблицы с JSON-данными.sql
@@ -8,3 +8,7 @@ CREATE TABLE candidate_profiles (
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
+
+SELECT r.res_full_name, p.additional_info
+FROM candidate_profiles p
+JOIN resumes r ON p.profile_resume_id = r.res_id;
